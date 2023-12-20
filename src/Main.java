d9a754bf75a0ce42b8e5aef78d138b4d9359a219
hgrafa <hugorafell@gmail.com>
2023-12-19 21:16:51 -0300

    refatorando services e testando na Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,43 @@
+import model.Empregado;
+import model.Terceirizado;
 import service.EmpregadoService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        EmpregadoService empregadoService = new EmpregadoService();
-        System.out.println("Hello world!");
+        List<Empregado> empregados = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        empregadoService.adicionarFuncionario("Mario",
-                10,
-                100.0,
-                'S');
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nome: ");
+            String nome = scanner.nextLine();
+            System.out.print("Valor p/ hora: ");
+            double valorPorHora = scanner.nextDouble();
+            System.out.print("Horas trabalhadas:");
+            int horasTrabalhadas = scanner.nextInt();
+
+            System.out.print("Eh terceirizado? (s/n): ");
+            boolean ehTerceirizado = scanner.next().equalsIgnoreCase("s");
+            double custoAdicional;
+
+            if(ehTerceirizado) {
+                System.out.print("Custo Adicional: ");
+                custoAdicional = scanner.nextDouble();
+                empregados.add(new Terceirizado(nome, horasTrabalhadas, valorPorHora, custoAdicional));
+            } else {
+                empregados.add(new Empregado(nome, horasTrabalhadas, valorPorHora));
+            }
+
+            System.out.println("-------------------");
+        }
 
     }
 
